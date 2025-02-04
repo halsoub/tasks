c326129e257cd625fd4c9bd2bbc4d12ae51a1af4
hebah <hebahalsoub20@gmail.com>
2025-02-04 13:16:29 +0300

    add task1 4Feb 2025 1:16

---
+++ tasks/lib/tasks/task1.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class BottomNavExample extends StatefulWidget {
+  const BottomNavExample({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavExample> createState() => _BottomNavExampleState();
+}
+
+class _BottomNavExampleState extends State<BottomNavExample> {
+  int _selectedIndex = 0;
+
+  static final List<Widget> _widgetOptions = <Widget>[
+    Container(
+        color: Colors.deepOrange, child: Center(child: Text('Home Page'))),
+    Container(
+        color: Colors.greenAccent, child: Center(child: Text('Search Page'))),
+    Container(
+        color: Colors.amberAccent, child: Center(child: Text('Profile Page'))),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bottom Navigation Example'),
+      ),
+      body: Center(
+        child: _widgetOptions[_selectedIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
