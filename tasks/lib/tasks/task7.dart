c80c72bf3aeb7f3438dc4db6745fbac64b9189d1
hebah <hebahalsoub20@gmail.com>
2025-02-06 15:07:57 +0300

    add task7 6 Feb 2025 3:07pm

---
+++ tasks/lib/tasks/task7.dart
@@ -0,0 +1,17 @@
+//Home page
+
+import 'package:flutter/material.dart';
+
+class Task7 extends StatefulWidget {
+  const Task7({super.key});
+
+  @override
+  State<Task7> createState() => _Task7State();
+}
+
+class _Task7State extends State<Task7> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container(child: Text("Home"),));
+  }
+}
