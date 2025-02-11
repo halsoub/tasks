87cd45cc5736e5ac8fa37c577262e69bd0aba33c
hebah <hebahalsoub20@gmail.com>
2025-02-11 14:17:17 +0300

    add task13 11 Feb 2025 2:17pm

--- tasks/lib/main.dart
+++ tasks/lib/main.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tasks/tasks/product_view_provider.dart';
 import 'package:tasks/tasks/task1.dart';
 
+import 'provider/product_provder.dart';
 import 'tasks/task10.dart';
+import 'tasks/task11.dart';
 import 'tasks/task5.dart';
 import 'tasks/task6.dart';
 import 'tasks/task8.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ProductProvider()),
+      ],
+      child: MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -17,7 +28,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Task10(),
+      home: ProductViewWidget(),
     );
   }
 }
