310839f13317ea0414771ec753facb436cb5eeb5
hebah <hebahalsoub20@gmail.com>
2025-02-27 11:41:23 +0300

    add task18 27Feb 2025 11:41pm

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:tasks/tasks/task17.dart';
 import 'provider/product_provder.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
